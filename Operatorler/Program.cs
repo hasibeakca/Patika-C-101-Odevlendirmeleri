b35492f6904fd18fb7538a874b441bb63c74ccef
Hasibe Akca <hasibetosun42@gmail.com>
2024-01-30 16:15:32 +0300

    Operatörler ödevi tamamlandı

--- Operatorler/Program.cs
+++ Operatorler/Program.cs
@@ -1,1 +1,58 @@
-﻿
+﻿int x = 3;
+int y = 3;
+y = y + 2;
+Console.WriteLine(y);
+y += 2;
+Console.WriteLine(y);
+y /= 1;
+Console.WriteLine(y);
+x *= 2;
+Console.WriteLine(x);
+
+//Mantıksal Operatörler
+// || && !
+
+bool isSuccess = true;
+bool isCompleted = true;
+
+if(isSuccess && isCompleted)
+    Console.WriteLine("Perfect!");
+if (isSuccess || isCompleted)
+    Console.WriteLine("Great");
+if (isSuccess &&  !isCompleted)
+    Console.WriteLine("Fine");
+//ilişkisel Operatörler
+//< , > , <= >= !=
+int a = 3;
+int b = 4;
+bool sonuc = a < b;
+Console.WriteLine(sonuc);
+sonuc = a>b;
+Console.WriteLine(sonuc);
+sonuc = a >= b;
+Console.WriteLine(sonuc);
+sonuc = a == b;
+Console.WriteLine(sonuc);
+sonuc = a != b;
+Console.WriteLine(sonuc);
+
+//Aritmetik operatörler
+// + , -, /, *
+
+int sayi = 10;
+int sayi1 = 5;
+int sonuc1 = sayi / sayi1;
+Console.WriteLine(sonuc1);
+int sonuc2 = sayi * sayi1;
+Console.WriteLine(sonuc2);
+int sonuc3 = sayi + sayi1;
+Console.WriteLine(sonuc3);
+int sonuc4 = sayi - sayi1;
+Console.WriteLine(sonuc4);
+int sonuc5 = sayi1++;
+Console.WriteLine(sonuc5);
+int sonuc6 = ++sayi1;
+Console.WriteLine(sonuc6);
+// % mod alır;
+int sonuc7= 20 % 3;
+Console.WriteLine(sonuc7);
--- Operatorler/obj/Debug/net8.0/Operatorler.AssemblyInfo.cs
+++ Operatorler/obj/Debug/net8.0/Operatorler.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Operatorler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1ec76807171bd01c91eec0d09104fa31d03f6b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Operatorler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Operatorler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
