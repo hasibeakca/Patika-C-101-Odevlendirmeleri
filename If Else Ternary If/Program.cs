09f12782683c7797ab3b88aecb611885cf09f637
Hasibe Akca <hasibetosun42@gmail.com>
2024-02-02 16:22:38 +0300

    ıf Else ternary If ödevi tamamlandı.

---
+++ If Else Ternary If/Program.cs
@@ -0,0 +1,16 @@
+﻿// Saate göre selamlama mesajı
+int time = DateTime.Now.Hour;
+
+if (time < 12)
+{
+    Console.WriteLine("Günaydın!");
+}
+else if (time >= 12 || time < 17)
+{
+    Console.WriteLine("İyi öğlenler!");
+} else if( time > 17)
+{
+    Console.WriteLine("İyi akşamlar!");
+}
+string sonuc = time < 12 ? "Günaydın" : time < 17 || time >= 12 ? "İyi Öğlenler" : "iyi Akşamlar";
+Console.WriteLine(sonuc);
