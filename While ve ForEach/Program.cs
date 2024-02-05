0cdd59b5335389e4866d09ae31c17821fc21e240
Hasibe Akca <hasibetosun42@gmail.com>
2024-02-05 20:38:28 +0300

    While ve ForEach ödevi tamamlandı.

---
+++ While ve ForEach/Program.cs
@@ -0,0 +1,25 @@
+﻿// 1 ile girilen sayı arasındaki sayı arasındaki sayıların ortalamasını hesaplama.
+
+int sayi = int.Parse(Console.ReadLine());
+int sayac = 1;
+int toplam = 0;
+while (sayac <= sayi)
+{
+   toplam += sayac;
+    sayac++;
+}
+Console.WriteLine(toplam/sayi);
+
+
+char karakter = 'a';
+while (karakter <= 'z')
+{
+    Console.Write(karakter);
+    karakter++;
+}
+Console.WriteLine("***********************Foreach*************");
+string[] arabalar = { "bmw", "toyota", "nissan", "ford" };
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
